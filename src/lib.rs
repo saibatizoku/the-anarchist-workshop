7655e55c43ed6688a3f479ca767ee537d0cf8303
Joaquín Rosales G <casabenita7@gmail.com>
2021-02-12 21:08:01 -0600

    depollute crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,5 @@
 //! # The Anarchist's Workshop
 //!
-//! This is a a bunch of code put together in order to run things at workshops.
-//!
-//! As in, places where people work. Workshops.
-//!
-//! The end goal is to have some Free Open-Source Software which can be installed
-//! relatively easy by Rust developers, which can configure/extend/modify pleasantly
-//! with good ol' Rust code, and install it on a computer from someone's grandparent
-//! and who has recently wanted to open a workshop for making grandparent-like goodies.
-//!
-//! It should be intuitive. Whatever that means.
-//!
-//! ## State of The Code
-//!
-//! This project is in the earliest of it's development. We've only got a few ideas
-//! and some time to spend on them.
-//!
-//! ## Motivation
-//!
-//! The title is inspired by the work of one Christopher M. Schwarz, well-known woodworker,
-//! bookwriter, publisher, etc. By reading his book about building his design for "The Anarchist Workbench".
-//! Yes, from reading his book. It's good, and it's licensed with Creative Commons over at the
-//! lostartpress.com, it's worth taking the time to look for it there, or somewhere in the
-//! blog.lostartpress.com.
 
 /// TheShop
 #[derive(Default, Debug, PartialEq)]
