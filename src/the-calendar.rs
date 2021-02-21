f0859225d72c0aabbd7d0cb36fee2b2619144ab4
Joaquín Rosales G <casabenita7@gmail.com>
2021-02-21 13:27:06 -0600

    add calendar tests to prototype functionality and ergonomics

--- src/the-calendar.rs
+++ src/the-calendar.rs
@@ -5,7 +5,7 @@
 //! Calendars are simple. Let's be the same way.
 //!
 //! At any given time, there can be many calendars around The Shop.
-use chrono::prelude::{Date, Utc};
+use chrono::prelude::{Date, Datelike, TimeZone, Utc};
 use std::collections::HashMap;
 
 /// TheCalendar
@@ -53,6 +53,13 @@ pub type ShopCalendars = HashMap<String, TheCalendar<CalendarPost>>;
 #[cfg(test)]
 mod tests {
     use super::*;
+    use chrono::Duration;
+
+    const YEAR: i32 = 2000;
+    const MONTH: u32 = 3;
+    const DAY: u32 = 8;
+    const POST_TEXT: &str = "Don't forget!";
+    const POST_EXPECTED_DISPLAY: &str = "2000-03-08: Don't forget!";
 
     #[test]
     fn the_calendar_should_implement_default_n_debug_n_partial_eq() {
@@ -68,5 +75,60 @@ mod tests {
     #[test]
     fn a_calendar_post_should_implement_default_n_debug_n_partial_eq() {
         assert_eq!(CalendarPost::default(), CalendarPost::default());
+    }
+
+    #[test]
+    fn a_new_calendar_post_has_todays_date() {
+        let post = CalendarPost::new("Remember to improve the calendar");
+        assert_eq!(post.date, Utc::now().date());
+    }
+
+    #[test]
+    fn a_new_calendar_post_can_set_its_date() {
+        let some_date = Utc.ymd(YEAR, MONTH, DAY);
+        let post = CalendarPost::new_with_date(POST_TEXT, some_date);
+        assert_eq!(post.date, some_date);
+    }
+
+    #[test]
+    fn a_new_calendar_post_can_be_created_with_year_month_day() {
+        let post = CalendarPost::new_with_ymd(POST_TEXT, YEAR, MONTH, DAY);
+        assert_eq!(post.date.year(), YEAR);
+        assert_eq!(post.date.month(), MONTH);
+        assert_eq!(post.date.day(), DAY);
+    }
+
+    #[test]
+    fn a_calendar_post_implements_fmt_display() {
+        // for our constant post info, we compare with the
+        // constant that has the expected display output
+        let post = CalendarPost::new_with_ymd(POST_TEXT, YEAR, MONTH, DAY);
+        assert_eq!(format!("{}", post), POST_EXPECTED_DISPLAY);
+
+        // for a date that is 5 days from today, we build the expected
+        // output by hand
+        let five_days_from_now = Utc::now().date() + Duration::days(5);
+        let (year, month, day) = (
+            five_days_from_now.year(),
+            five_days_from_now.month(),
+            five_days_from_now.day(),
+        );
+        let post = CalendarPost::new_with_date("Calendar continues", five_days_from_now);
+        let expected = format!("{}-{:0>2}-{:0>2}: {}", year, month, day, post.text);
+        assert_eq!(format!("{}", post), expected);
+    }
+
+    #[test]
+    fn a_calendar_with_two_posts() {
+        let five_days_from_now = Utc::now().date() + Duration::days(5);
+
+        let list_of_posts = vec![
+            CalendarPost::new("Calendar begins"),
+            CalendarPost::new_with_date("Calendar continues", five_days_from_now),
+        ];
+        let calendar = TheCalendar {
+            calendar_posts: AListOfPosts { list_of_posts },
+        };
+        assert_eq!(calendar.total_posts_count(), 2);
     }
 }
