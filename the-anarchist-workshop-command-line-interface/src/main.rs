19bc88099bc293d1b21068cb286a53a4418a69cf
Joaquín Rosales G <casabenita7@gmail.com>
2021-03-09 19:44:54 -0600

    use structopt

--- the-anarchist-workshop-command-line-interface/src/main.rs
+++ the-anarchist-workshop-command-line-interface/src/main.rs
@@ -1,10 +1,19 @@
 //! Command-line executable code for The Anarchist Workshop.
-use clap::App;
-use the_anarchist_workshop::the_calendar::TheCalendar;
+use structopt::StructOpt;
+//use the_anarchist_workshop::the_calendar::TheCalendar;
+
+pub const CLI_APP_NAME: &str = "taw";
+pub const CLI_APP_VERSION: &str = "v0.1.0";
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = CLI_APP_NAME, version = CLI_APP_VERSION)]
+pub struct TheCliApp {
+}
 
 /// This is the function that gets executed when you type `taw` on your terminal.
 /// Who knows? Maybe you want to spawn it on a thread somewhere. Maybe you want
 /// to stick to the main thread.
 pub fn main() {
-    App::new("taw").version("v0.1.0").get_matches();
+    let cli = TheCliApp::from_args();
+    println!("{:?}", cli);
 }
