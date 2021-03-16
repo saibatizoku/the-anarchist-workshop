0b53b75a7b8baab7e8b53d9efdd1fd9e7088bdaf
Joaquín Rosales G <casabenita7@gmail.com>
2021-03-15 23:31:17 -0600

    add 'commander' subcommand, stub text interface with cursive crate

--- the-anarchist-workshop-command-line-interface/src/main.rs
+++ the-anarchist-workshop-command-line-interface/src/main.rs
@@ -1,4 +1,5 @@
 //! Command-line executable code for The Anarchist Workshop.
+use cursive::views::{Dialog, TextView};
 use structopt::StructOpt;
 //use the_anarchist_workshop::the_calendar::TheCalendar;
 
@@ -8,6 +9,17 @@ pub const CLI_APP_VERSION: &str = "v0.1.0";
 #[derive(Debug, StructOpt)]
 #[structopt(name = CLI_APP_NAME, version = CLI_APP_VERSION)]
 pub struct TheCliApp {
+    #[structopt(subcommand)]
+    cmd: Option<TAWcommands>,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum TAWcommands {
+    #[structopt(
+        about = "Run the interactive interface on your shell",
+        long_about = "Reminds me of the midnight commander, but less cool."
+    )]
+    Commander,
 }
 
 /// This is the function that gets executed when you type `taw` on your terminal.
@@ -16,4 +28,26 @@ pub struct TheCliApp {
 pub fn main() {
     let cli = TheCliApp::from_args();
     println!("{:?}", cli);
+    match cli.cmd {
+        Some(cmd) => {
+            match cmd {
+                TAWcommands::Commander => {
+                    {
+                        let mut siv = cursive::default();
+
+                        siv.add_layer(
+                            Dialog::around(TextView::new("TAW"))
+                                .title("The Anarchist's Workshop")
+                                .button("Salir", |s| s.quit()),
+                        );
+
+                        siv.run();
+                    }
+                }
+            }
+        }
+        None => {
+            println!("Type \"taw -h\" or \"taw --help\" to begin.")
+        }
+    }
 }
